5fb35e6244d563d15bb524bd3b79b53c2e8ae204
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2022-02-04 14:20:12 -0500

    Updated the PCA functions to prepare for batch_pca
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/PCA.R
+++ R/PCA.R
@@ -1,11 +1,26 @@
 #' Produce a PCA plot from txi
 #'
+#' The PCA is produced using the TPM by default (can also be the ruvg counts;
+#' see the \code{ruvg_normalization} param)
+#'
 #' @param txi The \code{txi} object returned by the \code{import_kallisto}
 #' function.
 #' @param graph Produce the graph. \code{TRUE} or \code{FALSE}. Default:
 #' \code{TRUE}.
 #' @param use_ruv Use RUVg normalization? Needs to be pre-computed using the
-#'                \code{ruvg_normalization} function. Default: \code{FALSE}.
+#' \code{ruvg_normalization} function. Default: \code{FALSE}.
+#' @param min_counts The minimal mean number of count (TPM by default; see also
+#' the \code{ruvg_normalization} param) required to keep the feature (gene or
+#' transcript) for the PCA.
+#' @param metatada The metadata to add to the coordinate table to add color or
+#' shape with the results and the \code{plot_pca} function. If there is no
+#' metadata available, keep the default value of \code{NULL}. Value can either
+#' be a csv file or a \code{data.frame}. Default: \code{NULL}.
+#' @param id_metadata The colname to use to join the metadata with the
+#' coordinate table. Must contains all the sample names found in the txi. If
+#' \code{NULL} and a metadata table is provided, the first column will be used
+#' to join the tables. Default: \code{NULL}.
+#' @param ncp Number of component to include in the graph. Default: 2.
 #'
 #' @return Produce the PCA and silently returns the \code{data.frame} and the
 #' result from the \code{PCA} function as a \code{list} with 2 elements
@@ -13,7 +28,7 @@
 #'
 #' @examples
 #' txi <- get_demo_txi()
-#' df <- produce_pca(txi, graph = FALSE)
+#' df <- produce_pca_df(txi)
 #'
 #' @importFrom magrittr %>%
 #' @importFrom FactoMineR PCA
@@ -28,7 +43,28 @@
 #' @importFrom utils tail
 #'
 #' @export
-produce_pca <- function(txi, graph = TRUE, use_ruv = FALSE) {
+produce_pca_df <- function(txi, use_ruv = FALSE, min_counts = 5,
+                           metadata = NULL, id_metadata = NULL, ncp = 2) {
+
+    validate_txi(txi)
+    stopifnot(is(use_ruv, "logical"))
+    if (!is.null(metadata)) {
+        stopifnot(is(metadata, "data.frame") | is(metadata, "character"))
+        if (is.character(metadata)) {
+            stopifnot(file.exists(metadata))
+            metadata <- readr::read_csv(metadata, show_col_types = FALSE)
+        }
+        if (is.null(id_metadata)) {
+            id_metadata <- colnames(metadata)[1]
+        }
+        stopifnot(is(id_metadata, "character"))
+        stopifnot(id_metadata %in% colnames(metadata))
+        stopifnot(all(colnames(txi$abundance) %in% metadata[[id_metadata]]))
+    }
+    stopifnot(is(ncp, "numeric"))
+    stopifnot(identical(ncp, round(ncp)))
+    stopifnot(ncp > 1)
+
     if (!use_ruv) {
         tpm <- as.data.frame(txi$abundance)
     } else {
@@ -36,13 +72,14 @@ produce_pca <- function(txi, graph = TRUE, use_ruv = FALSE) {
         stopifnot(is(txi$ruvg_counts, "matrix"))
         tpm <- as.data.frame(txi$ruvg_counts)
     }
+
     tpm <- tpm %>%
             dplyr::mutate(ensembl_gene = rownames(tpm)) %>%
             tidyr::gather(sample, tpm, -ensembl_gene)
 
     min_tpm <- dplyr::group_by(tpm, ensembl_gene) %>%
         dplyr::summarize(tpm = sum(tpm)) %>%
-        dplyr::filter(tpm >= 5) %>%
+        dplyr::filter(tpm >= min_counts) %>%
         dplyr::pull(ensembl_gene)
 
     tpm_filter <- dplyr::filter(tpm, ensembl_gene %in% min_tpm)
@@ -58,59 +95,167 @@ produce_pca <- function(txi, graph = TRUE, use_ruv = FALSE) {
     coord <- pca$ind$coord
     dims <- stringr::str_extract(colnames(coord), "[0-9]*$") %>%
         as.numeric
-    max_dim <- min(tail(dims, 1), 5)
+    stopifnot(ncp <= tail(dims, 1))
     df <- data.frame(Dim1 = coord[,1])
-    for (i in seq(2, max_dim)) {
+    for (i in seq(2, ncp)) {
         df[[paste0("Dim", i)]] <- coord[,i]
     }
     df <- df %>%
         dplyr::mutate(sample = rownames(df))
-    res <- list(df = df, pca = pca)
 
-    p <- plot_pca(res)
-    if (graph) print(p)
+    if (!is.null(metadata)) {
+        if (is.null(id_metadata)) {
+            id_metadata <- colnames(metadata)[1]
+        }
+        df <- left_join(df, metadata, by = c("sample" = id_metadata))
+    }
 
-    invisible(res)
+    xlab <- paste0("Dim1 (", pca$eig[1,2] %>% round(2), "%)")
+    ylab <- paste0("Dim2 (", pca$eig[2,2] %>% round(2), "%)")
+
+    list(coord = df, xlab = xlab, ylab = ylab)
 }
 
-#' Produce a PCA plot from produce_pca results
+#' Produce a PCA plot from produce_pca_df results
 #'
-#' @param res_pca The \code{list} returned from the produce_pca function
-#' @param color The name of the column in \code{res_pca$df} to use to color the
+#' @param res_pca The \code{list} returned from the \code{produce_pca} function
+#' @param size The size of the points. Default: 3.
+#' @param color The name of the column in \code{res_pca$coord} to use to color the
 #' points in the PCA. If \code{NULL}, won't add color. Default: \code{NULL}.
+#' @param shape The name of the column in \code{res_pca$coord} to use to define
+#' the shape of the points in the PCA. If \code{NULL}, won't add change the shape.
+#' Default: \code{NULL}.
+#' @param show_names Try to add the labels of each point? Will only add the
+#' labels if the points are not too closely clustered together in the PCA.
+#' Default: \code{TRUE}.
+#' @param title Add a title to the graph? If \code{NULL}, no title is added to
+#' the graph. Otherwise this param must be a \code{character}. Default:
+#' \code{NULL}.
+#' @param graph Produce the graph. \code{TRUE} or \code{FALSE}. Default:
+#' \code{TRUE}.
+#' @param legend.position Value for the \code{legend.position} param from
+#' ggplot2. Default: "right".
+#' @param legend.box Value for the \code{legend.box} param from ggplot2.
+#' Default: "vertical".
 #'
 #' @return Returns the \code{ggplot} object
 #'
 #' @examples
 #' txi <- get_demo_txi()
-#' res_pca <- produce_pca(txi, graph = FALSE)
-#' p <- plot_pca(res_pca)
+#' res_pca <- produce_pca_df(txi)
+#' p <- plot_pca(res_pca, graph = FALSE)
 #'
+
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
 #' @importFrom ggplot2 aes_string
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 theme_bw
-#' @importFrom ggplot2 xlab
-#' @importFrom ggplot2 ylab
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 scale_shape_manual
+#' @importFrom ggplot2 ggtitle
 #' @importFrom ggrepel geom_text_repel
 #'
 #' @export
-plot_pca <- function(res_pca, color = NULL) {
-    if (is.null(color)) {
-        p <- ggplot2::ggplot(res_pca$df, ggplot2::aes(x = Dim1, y = Dim2))
-    } else {
-        p <- ggplot2::ggplot(res_pca$df,
-                             ggplot2::aes_string(x = "Dim1",
-                                                 y = "Dim2",
-                                                 color = color))
+plot_pca <- function(res_pca, size = 3, color = NULL, shape = NULL,
+                     show_names = TRUE, title = NULL, graph = TRUE,
+                     legend.position = "right",
+                     legend.box = "vertical") {
+
+    # Validate the params
+    stopifnot(is(res_pca, "list"))
+    stopifnot(all(c("coord", "xlab", "ylab") %in% names(res_pca)))
+    stopifnot(is(res_pca$coord, "data.frame"))
+    stopifnot(nrow(res_pca$coord) > 0)
+    stopifnot(all(c("Dim1", "Dim2", "sample") %in% colnames(res_pca$coord)))
+
+    stopifnot(is(res_pca$xlab, "character"))
+    stopifnot(is(res_pca$ylab, "character"))
+
+    stopifnot(is(size, "numeric"))
+    stopifnot(identical(size, round(size)))
+    stopifnot(size > 0)
+
+    if (!is.null(color)) {
+        stopifnot(is(color, "character"))
+        stopifnot(color %in% colnames(res_pca$coord))
     }
-    p + ggplot2::geom_point(size = 3) +
-        ggrepel::geom_text_repel(ggplot2::aes(label = sample),
-                                 color = "black", force = 10) +
+    if (!is.null(shape)) {
+        stopifnot(is(shape, "character"))
+        stopifnot(shape %in% colnames(res_pca$coord))
+    }
+    stopifnot(is(show_names, "logical"))
+    if (!is.null(title)) {
+        stopifnot(is(title, "character"))
+    }
+    stopifnot(is(graph, "logical"))
+
+    # To avoid 6 shapes limitation
+    if (!is.null(shape)) {
+        shape_vec <- c(15,16,17,18,19,20,1,2,3,4,5,6,7,8,9,10,11,12,13,14)
+        shape_vec <- shape_vec[1:length(unique(res_pca$coord[[shape]]))]
+    }
+
+    # Produce the graph
+    gg <- ggplot2::ggplot(res_pca$coord, ggplot2::aes(x = Dim1, y = Dim2)) +
         ggplot2::theme_bw() +
-        ggplot2::xlab(paste0("Dim1 (",
-                             res_pca$pca$eig[1,2] %>% round(2), "%)")) +
-        ggplot2::ylab(paste0("Dim2 (",
-                             res_pca$pca$eig[2,2] %>% round(2), "%)"))
+        ggplot2::labs(x = res_pca$xlab, y = res_pca$ylab)
+
+    ## Add color and shape
+    if (!is.null(color) & !is.null(shape)) {
+        gg <- gg +
+            ggplot2::geom_point(ggplot2::aes_string(col = color, shape = shape),
+                                size = size) +
+            ggplot2::scale_shape_manual(values = shape_vec)
+    } else if (!is.null(color) & is.null(shape)) {
+        gg <- gg +
+            ggplot2::geom_point(ggplot2::aes_string(col = color), size = size)
+    } else if (is.null(color) & !is.null(shape)) {
+        gg <- gg +
+            ggplot2::geom_point(ggplot2::aes_string(shape = shape), size = size) +
+            ggplot2::scale_shape_manual(values = shape_vec)
+    } else {
+        gg <- gg + ggplot2::geom_point(size = size)
+    }
+
+    # legend
+    if (!is.null(color) | !is.null(shape)) {
+        gg <- gg + ggplot2::theme(legend.position = legend.position,
+                                  legend.box = legend.box)
+    }
+
+    # show_names
+    if(!is.null(show_names)){
+        gg <- gg + ggrepel::geom_text_repel(ggplot2::aes(label = sample),
+                                            color = "black", force = 10)
+    }
+
+    # title
+    if (!is.null(title)) {
+        gg <- gg + ggplot2::ggtitle(title)
+    }
+
+    # graph
+    if (graph) {
+        print(gg)
+    }
+
+    invisible(gg)
+}
+
+#' Deprecated
+#'
+#' @param txi The \code{txi} object returned by the \code{import_kallisto}
+#' function.
+#' @param graph Produce the graph. \code{TRUE} or \code{FALSE}. Default:
+#' \code{TRUE}.
+#' @param use_ruv Use RUVg normalization? Needs to be pre-computed using the
+#' \code{ruvg_normalization} function. Default: \code{FALSE}.
+#'
+#' @export
+produce_pca <- function(txi, graph = TRUE, use_ruv = FALSE) {
+    msg <- paste0("This function is now deprecated, please use the ",
+                  "`produce_pca_df` and the `plot_pca` functions to produce ",
+                  "your PCA.")
+    message(msg)
 }
---
+++ R/txi.R
@@ -0,0 +1,31 @@
+validate_txi <- function(txi) {
+    # Global
+    stopifnot(is(txi, "list"))
+    stopifnot(all(c("counts", "abundance", "length", "anno") %in% names(txi)))
+
+    # Matrices
+    stopifnot(is(txi$counts, "matrix"))
+    stopifnot(is(txi$abundance, "matrix"))
+    stopifnot(is(txi$length, "matrix"))
+    stopifnot(is.numeric(txi$counts))
+    stopifnot(is.numeric(txi$abundance))
+    stopifnot(is.numeric(txi$length))
+    stopifnot(identical(colnames(txi$counts), colnames(txi$abundance)))
+    stopifnot(identical(colnames(txi$counts), colnames(txi$length)))
+    stopifnot(identical(rownames(txi$counts), rownames(txi$abundance)))
+    stopifnot(identical(rownames(txi$counts), rownames(txi$length)))
+
+    if (!is.null(txi$ruvg_counts)) {
+        stopifnot(is(txi$ruvg_counts, "matrix"))
+        stopifnot(is.numeric(txi$ruvg_counts))
+        stopifnot(identical(colnames(txi$ruvg_counts), cownames(txi$counts)))
+        stopifnot(identical(rolnames(txi$ruvg_counts), rownames(txi$counts)))
+    }
+
+    # Data.frame
+    stopifnot(is(txi$anno, "data.frame"))
+    expected_col <- c("id", "ensembl_gene", "symbol", "entrez_id", "transcript_type")
+    stopifnot(expected_col %in% colnames(txi$anno))
+    stopifnot(identical(txi$anno$id, rownames(txi$counts)))
+    invisible(TRUE)
+}
