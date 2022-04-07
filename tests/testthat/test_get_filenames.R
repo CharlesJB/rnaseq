fd63c0b3728aaa9995a8c9507e5c919b2fa12589
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2022-04-07 08:43:07 -0400

    Removed empty h5 files that caused some tests to crash
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- tests/testthat/test_get_filenames.R
+++ tests/testthat/test_get_filenames.R
@@ -3,7 +3,7 @@ library(rnaseq)
 test_that("Correct params values works correctly", {
     # Correct dir_kallisto
     dir_quant <- get_demo_kallisto_dir()
-    filenames <- get_filenames(dir_quant)
+    filenames <- get_filenames(dir_quant, file_extension = "tsv")
     expect_true(all(file.exists(filenames)))
 
     # Correct file_extension
@@ -23,7 +23,7 @@ test_that("Invalid file_extension throw error", {
     expect_error(get_filenames(get_demo_kallisto_dir(), NA), msg, fixed = TRUE)
 
     msg <- 'length(file_extension) == 1 is not TRUE'
-    expect_error(get_filenames(get_demo_kallisto_dir(), c("h5", "a")), msg, fixed = TRUE)
+    expect_error(get_filenames(get_demo_kallisto_dir(), c("tsv", "a")), msg, fixed = TRUE)
 
     msg <- 'file_extension %in% c("h5", "tsv") is not TRUE'
     expect_error(get_filenames(get_demo_kallisto_dir(), "a"), msg, fixed = TRUE)
